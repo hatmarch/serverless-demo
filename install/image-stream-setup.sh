1b9fa8a4ce831dcab9a86e8a8ab0cb86a00dc04f
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2019-12-10 12:18:54 +0000

    update and fix walkthrough to deal with inability to hit imagestream by label
    update link for renamed setup file
    
    updates to the revision section to clean up tagging

--- install/image-stream-setup.sh
+++ install/image-stream-setup.sh
@@ -6,6 +6,10 @@ oc tag --source docker mhildema/inventory:v1 inventory:initial-build
 oc tag --source docker mhildema/catalog:v1 catalog:initial-build
 oc tag --source docker mhildema/order:v1 order:initial-build
 
+# Seems that some time needs to pass otherwise openshift will think the tag doesn't point to an image and then the 
+# tagging of latest will fail (which in turn will cause deployment to fail)
+sleep 5
+
 # Now trigger builds by setting the latest tag in the image stream (which the DeploymentConfigs should be keyed to)
 oc tag payment:initial-build payment:latest
 oc tag cart:initial-build cart:latest
