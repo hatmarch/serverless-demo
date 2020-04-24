5c248dc9159dba62beb79c7f134a49bdbf44464e
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-04-24 03:28:01 +0000

    Add example of updating payments topic from command line

--- scripts/install-payment.sh
+++ scripts/install-payment.sh
@@ -24,6 +24,8 @@ oc rollout status -w dc/payment
 # # example sending messages (from here: https://kafka.apache.org/quickstart#quickstart_send)
 # echo '{"orderId": "12321","total": "232.23", "creditCard": {"number":"4232454678667866","expiration": "04/22","nameOnCard": "Jane G Doe"}, "billingAddress": "123 Anystreet, Pueblo, CO 32213", "name": "Jane Doe"}' | oc exec -i -c kafka my-cluster-kafka-0 -n user1-cloudnativeapps -- /opt/kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic orders
 
+# echo '{"orderId":"order-1919-id-0.17061231920733744","paymentId":"27369","remarks":"Payment of 31.2325 succeeded for Joe Bloggs CC details: {\"number\":\"41234123412341234\",\"expiration\":\"12/20\",\"nameOnCard\":\"Joe Bloggs\"}","status":"COMPLETED (Quarkus Local)"}' | oc exec -i -c kafka my-cluster-kafka-0 -n coolstore -- /opt/kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic payments
+
 # # example receiving messages (from previous)
 # oc exec -c kafka my-cluster-kafka-0 -n user1-cloudnativeapps -- /opt/kafka/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic orders --from-beginning
 # oc exec -c kafka my-cluster-kafka-0 -n user1-cloudnativeapps -- /opt/kafka/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic payments --from-beginning
