e914d68ae48a8aa08196c9da5b8efba898a4ac6a
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2021-01-05 13:35:10 +0000

    Update install-prereq script for new serverless
    Updates name of kafka project
    
    kafka-eventing points to proper kafka cluster bootstrap
    
    Adds option to skip installation of serverless operator

--- scripts/install-prereq.sh
+++ scripts/install-prereq.sh
@@ -4,12 +4,17 @@ set -e -u -o pipefail
 
 declare SKIP_KAFKA_EVENTING=""
 declare FOR_CRC=""
-declare PROJECT="user1-cloudnativeapps"
+declare KAFKA_PROJECT="kn-demo-dev"
+declare INSTALL_SERVERLESS="true"
 
 while (( $# )); do
     case "$1" in
         --skip-knative-kafka-eventing)
             SKIP_KAFKA_EVENTING="true"
+            shift
+            ;;
+        --skip-serverless)
+            INSTALL_SERVERLESS=""
             shift
             ;;
         --crc)
@@ -21,14 +26,14 @@ while (( $# )); do
             exit 1
             ;;
         *)
-            PROJECT=$1
+            KAFKA_PROJECT=$1
             shift
             ;;
     esac
 done
 
-oc get ns $PROJECT 2>/dev/null  || { 
-    oc new-project $PROJECT 
+oc get ns $KAFKA_PROJECT 2>/dev/null  || { 
+    oc new-project $KAFKA_PROJECT 
 }
 
 command.wait_for_crd()
@@ -52,19 +57,17 @@ command.wait_for_crd()
 #
 
 # install the serverless operator
-oc apply -f "$DEMO_HOME/install/serverless/subscription.yaml" 
+if [[ -n "$INSTALL_SERVERLESS" ]]; then
+    echo "Installing Serverless Operator"
+    oc apply -f "$DEMO_HOME/install/serverless/subscription.yaml" 
+else
+    echo "Skipping installation of Serverless Operator"
+fi
 
 # install the kafka operator (AMQStreams)
 oc apply -f "$DEMO_HOME/install/kafka/subscription.yaml" 
 
 # NOTE: knative eventing operator is now included as part of the OpenShift serverless operator
-
-#install the kafka knative eventing operator
-if [ -z "$SKIP_KAFKA_EVENTING" ]; then
-    oc apply -f "$DEMO_HOME/install/kafka-eventing/subscription.yaml"
-else
-    echo "SKIPPING installation of kafka eventing at user's request."
-fi
 
 #
 # Install Kafka Instances
@@ -82,36 +85,37 @@ else
     oc process -f "$DEMO_HOME/install/kafka/kafka-template.yaml" -p REPLICA_COUNT=1 -p MIN_ISR=1 | oc apply -f -
 fi
 
-# install the necessary kafka instance and topics
-oc apply -f "$DEMO_HOME/install/kafka/kafka-order-topic.yaml" -n $PROJECT
-oc apply -f "$DEMO_HOME/install/kafka/kafka-payment-topic.yaml" -n $PROJECT
+# # install the necessary kafka instance and topics
+# oc apply -f "$DEMO_HOME/install/kafka/kafka-order-topic.yaml" -n $PROJECT
+# oc apply -f "$DEMO_HOME/install/kafka/kafka-payment-topic.yaml" -n $PROJECT
 
 #
 # Install Serving
 #
+if [[ -n "$INSTALL_SERVERLESS" ]]; then
+    echo "Waiting for the operator to install the Knative CRDs"
+    command.wait_for_crd "crd/knativeservings.operator.knative.dev"
 
-echo "Waiting for the operator to install the Knative CRDs"
-command.wait_for_crd "crd/knativeservings.operator.knative.dev"
+    oc apply -f "$DEMO_HOME/install/serverless/cr.yaml"
 
-oc apply -f "$DEMO_HOME/install/serverless/cr.yaml"
+    echo "Waiting for the knative serving instance to finish installing"
+    oc wait --for=condition=InstallSucceeded knativeserving/knative-serving --timeout=6m -n knative-serving
 
-echo "Waiting for the knative serving instance to finish installing"
-oc wait --for=condition=InstallSucceeded knativeserving/knative-serving --timeout=6m -n knative-serving
+    #
+    # Install Knative Eventing
+    #
+    echo "Waiting for the operator to install the Knative Event CRD"
+    command.wait_for_crd "crd/knativeeventings.operator.knative.dev"
 
-#
-# Install Knative Eventing
-#
-echo "Waiting for the operator to install the Knative Event CRD"
-command.wait_for_crd "crd/knativeeventings.operator.knative.dev"
-
-oc apply -f "$DEMO_HOME/install/knative-eventing/knative-eventing.yaml" 
-echo "Waiting for the knative eventing instance to finish installing"
-oc wait --for=condition=InstallSucceeded knativeeventing/knative-eventing -n knative-eventing --timeout=6m
+    oc apply -f "$DEMO_HOME/install/knative-eventing/knative-eventing.yaml" 
+    echo "Waiting for the knative eventing instance to finish installing"
+    oc wait --for=condition=InstallSucceeded knativeeventing/knative-eventing -n knative-eventing --timeout=6m
+fi
 
 
 if [ -z "$SKIP_KAFKA_EVENTING" ]; then
     # This where kafka eventing would be installed
-    oc apply -f "$DEMO_HOME/install/kafka-eventing/kafka-eventing.yaml"
+    sed "s/%KAFKA_PRJ%/${KAFKA_PROJECT}/g" "$DEMO_HOME/install/knative-eventing/kafka-eventing.yaml" | oc apply -f -
 else
     echo "Skipping Kafka Eventing configuration at the user's request"
 fi
@@ -122,5 +126,5 @@ fi
 
 # wait until the cluster is deployed
 echo "Waiting up to 30 minutes for kafka cluster to be ready"
-oc wait --for=condition=Ready kafka/my-cluster --timeout=30m -n $PROJECT
+oc wait --for=condition=Ready kafka/my-cluster --timeout=30m -n $KAFKA_PROJECT
 echo "Kafka cluster is ready."
